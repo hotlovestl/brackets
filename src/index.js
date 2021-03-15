b50cb638700825d2917dd0010027a100e8030c2a
Artiom Uradau <70811102+hotlovestl@users.noreply.github.com>
2021-03-16 02:46:00 +0300

    init: start project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = bracketsConfig.map(element => element.join(''));
+  for (let i = 0; i < arr.length; i++){
+    if(str.includes(arr[i])){
+      str = str.split(arr[i]).join('');
+      i = -1;
+    }
+  }
+  if (str == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
